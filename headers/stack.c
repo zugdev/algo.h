d87f033d479228a0a1083558294773ed437fce32
zugdev <zug.dev@proton.me>
2024-08-30 11:14:39 -0300

    feat: add stack header

---
+++ headers/stack.c
@@ -0,0 +1,32 @@
+#include "stack.h"
+#include <stdlib.h>
+
+Stack* createStack(int capacity) {
+  Stack* stack = (Stack*)malloc(sizeof(Stack));
+  stack->elements = (Node**)malloc(sizeof(Node*) * capacity);
+  stack->top = -1;
+  stack->capacity = capacity;
+  return stack;
+}
+
+void push(Stack* stack, Node* element) {
+  if (stack->top < stack->capacity - 1) {
+    stack->elements[++stack->top] = element;
+  }
+}
+
+Node* pop(Stack* stack) {
+  if (stack->top >= 0) {
+    return stack->elements[stack->top--];
+  }
+  return NULL;
+}
+
+int isStackEmpty(Stack* stack) {
+  return stack->top == -1;
+}
+
+void freeStack(Stack* stack) {
+  free(stack->elements);
+  free(stack);
+}
---
+++ headers/stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "graph.h"
+
+typedef struct Stack {
+  Node** elements;
+  int top;
+  int capacity;
+} Stack;
+
+Stack* createStack(int capacity);
+void push(Stack* stack, Node* element);
+Node* pop(Stack* stack);
+int isStackEmpty(Stack* stack);
+void freeStack(Stack* stack);
+
+#endif  // STACK_H
