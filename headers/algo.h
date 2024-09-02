76c7b456948c66110003a825bfd1cabf42a8d5fd
zugdev <zug.dev@proton.me>
2024-09-02 15:33:10 -0300

    feat: add new functions to header

--- headers/algo.h
+++ headers/algo.h
@@ -6,7 +6,10 @@
 #include "stack.h"
 
 void dfs_visit(Node* node, Stack* stack, int time);
-Stack* dfs(Graph* graph);
+Stack* dfs(Graph* graph, int time);
 void bfs(Graph* graph, Node* start);
-
+void kosajaru(Graph* graph);
+int dfs_example();
+int bfs_example();
+int kosajaru_example();
 #endif  // ALGO_H
