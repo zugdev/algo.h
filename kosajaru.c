992b27f05ed1f9dd69c8bc97dae4267f364aa1a4
zugdev <zug.dev@proton.me>
2024-09-05 10:41:25 -0300

    chore: comments

--- kosajaru.c
+++ kosajaru.c
@@ -5,9 +5,32 @@
 #include "headers/graph.h"
 #include "headers/stack.h"
 
+/*
+ * Kosaraju's Algorithm for finding Strongly Connected Components (SCCs) in a
+ * directed graph:
+ *
+ * 1. Perform a DFS on the original graph to compute the finishing times of the
+ * nodes. The nodes are pushed onto a stack in the order of their finishing
+ * times.
+ *
+ * 2. Invert the graph (reverse all the edges).
+ *
+ * 3. Perform a second DFS on the inverted graph, processing the nodes in the
+ * order defined by the stack (which represents the nodes in the order of
+ * decreasing finishing times).
+ *
+ * 4. Each DFS run in the inverted graph will discover a strongly connected
+ * component. The nodes discovered in each DFS run form one SCC, and all SCCs
+ * are printed.
+ *
+ * The algorithm efficiently finds all SCCs in O(V + E) time, where V is the
+ * number of vertices and E is the number of edges in the graph.
+ */
+
 void kosaraju(Graph* graph) {
   int time = 0;
   Stack* stack = dfs(graph, time);            // first DFS to fill the stack
+  printStack(stack);
   Graph* inverted_graph = invertArcs(graph);  // invert the graph edges
 
   // reset node colors for the second DFS
@@ -15,7 +38,7 @@ void kosaraju(Graph* graph) {
     inverted_graph->nodes[i]->color = WHITE;
   }
 
-  printf("Strongly connected components:\n");
+  printf_green("\nStrongly Connected Components:\n");
 
   int component_number = 1;
   // second DFS based on the finishing times from the first DFS
@@ -35,7 +58,7 @@ void kosaraju(Graph* graph) {
 }
 
 int kosajaru_example() {
-  printf_cyan("kosajaru example\n");
+  printf_cyan("kosajaru example\n\n");
   Node* a = createNode('a');
   Node* b = createNode('b');
   Node* c = createNode('c');
