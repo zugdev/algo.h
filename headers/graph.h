c7982d0766f0c780150db59eb1dfbef27ac803bc
zugdev <zug.dev@proton.me>
2024-09-20 18:17:18 -0300

    feat: comments

--- headers/graph.h
+++ headers/graph.h
@@ -4,15 +4,17 @@
 typedef enum { WHITE, GRAY, BLACK } Color;
 
 typedef struct Node {
-  char name;
-  int id;
-  int distance;
-  int t_entering;
-  int t_leaving;
-  Color color;
-  struct Node** neighbors;
-  int out_degree;
-  struct Node* parent;
+  char name;                  // this is for printing only
+  int id;                     // node's backend identifier
+  int distance;               // distance from an arbitrary root
+  int t_entering;             //  time entering the node for dfs
+  int t_leaving;              // time leaving the node for dfs
+  Color color;                // color for algos like bfs, dfs
+  struct Node** neighbors;    //  list of adjacency
+  int out_degree;             // number of edges leaving the graph (directed or not
+  struct Node* parent;        // direct parent
+  int low;                    // lowest point reachable from this node
+  int is_articulation_point;  // flag for articulation points
 } Node;
 
 typedef struct Graph {
